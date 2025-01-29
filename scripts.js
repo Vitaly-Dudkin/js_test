9e06283d89645336bb2d1a8694d27e9fc8c4844b
Виталий <dudkin231@mail.ru>
2025-01-29 12:15:58 +0300

    frist hw

---
+++ scripts.js
@@ -0,0 +1,30 @@
+// task #1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+
+// task #2
+let yearOfCreation = 2009
+alert(yearOfCreation)
+
+// task #3
+let nameJS = 'bob'
+alert(nameJS)
+
+// task #4
+let num = 10;
+let num_2 = 2;
+alert(`${num + num_2}, ${num - num_2}`);
+
+// task #5
+let result = 2 ** 5;
+alert(result);
+
+// task #6
+
+let c = 9;
+let b = 2;
+alert(`${c % b}`)
+
+
