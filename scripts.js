013c48114bc8efec0855a027d90925949b786b35
Виталий <dudkin231@mail.ru>
2025-01-30 16:56:21 +0300

    some chages

--- scripts.js
+++ scripts.js
@@ -1,30 +1,20 @@
-// task #1
-let a = 10;
-alert(a);
-a = 20;
-alert(a);
-
-// task #2
-let yearOfCreation = 2009
-alert(yearOfCreation)
-
-// task #3
-let nameJS = 'bob'
-alert(nameJS)
-
-// task #4
-let num = 10;
-let num_2 = 2;
-alert(`${num + num_2}, ${num - num_2}`);
-
-// task #5
-let result = 2 ** 5;
-alert(result);
-
-// task #6
-
-let c = 9;
-let b = 2;
-alert(`${c % b}`)
-
-
+function getSeason() {
+    let userinpit = Number(prompt('Input number'))
+    let digitsWinter = [12, 1, 2]
+    let digitsSpring = [3,4,5]
+    let digitsSummer = [6,7,8]
+    let digitsAutmum = [9,10, 11]
+    if (digitsWinter.includes(userinpit)) {
+        alert('winter')
+    }
+    else if (digitsSpring.includes(userinpit)) {
+        alert('autum')
+    }
+    else if (digitsSummer.includes(userinpit)) {
+        alert('summer')
+    } else if (digitsAutmum.includes(userinpit)) {
+        alert('autum')
+    } else {
+        alert('number can be less than 13 and more than 0')
+    }
+}
